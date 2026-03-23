9353aae52f8a22846ae46016b2ccf4d5b614615f
Ravi Kumar Gupta <87311770+rk-1402@users.noreply.github.com>
2026-03-23 23:53:05 +0530

    Update ec2-upload.py

--- ec2-upload.py
+++ ec2-upload.py
@@ -1,1 +1,13 @@
+import boto3
 
+s3 = boto3.client('s3')
+
+bucket = "my-s3-project-input"
+file_name = "test.txt"
+
+with open(file_name, "w") as f:
+    f.write("hello from ec2")
+
+s3.upload_file(file_name, bucket, file_name)
+
+print("File uploaded successfully!")
